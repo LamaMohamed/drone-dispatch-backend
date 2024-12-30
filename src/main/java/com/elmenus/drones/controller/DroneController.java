a4a75bd07f7da847471e60c3df6d195c11944e98
lamaabdou <lamaabdou96@gmail.com>
2024-12-30 22:19:53 +0200

    update api endpoint, and readme file

--- src/main/java/com/elmenus/drones/controller/DroneController.java
+++ src/main/java/com/elmenus/drones/controller/DroneController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("${api.drone}")
+@RequestMapping("${api.drones}")
 @Slf4j
 public class DroneController {
 
